d41ac84f8a085b6859652a5a1632122b2f0068d2
DracoClaw <34274162+DracoClaw@users.noreply.github.com>
2023-11-14 01:18:49 +0200

    Fix mod import and handle select change in app component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import Config from './mod';
+import Config from './config';
 import { mods } from '../mods/mods';
 
 function App() {
   const [selectedMod, setSelectedMod] = useState('');
 
-  const onChange = (e) => {
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedMod(e.target.value);
   };
 
   return (
     <React.StrictMode>
+      <div style={{ padding: '20px' }}>
       <h1>SRML Config Tool</h1>
-      <select onChange={onChange}>
+      <select onChange={handleSelectChange}>
         {!selectedMod && <option value="null">Select a mod</option>}
         {Object.keys(mods).map((modName) => ( 
           <option key={modName} value={modName}>
@@ -20,7 +21,8 @@ function App() {
           </option>
         ))}
       </select>
-      <Config modSelected={selectedMod} onChange={onChange}/>
+      <Config modSelected={selectedMod}/>
+      </div>
     </React.StrictMode>
   );
 }
--- src/components/config.tsx
+++ src/components/config.tsx
@@ -2,19 +2,20 @@ import React, { FC, useState, useEffect } from 'react';
 import { mods } from '../mods/mods';
 
 interface Props {
-  modSelected: string | keyof typeof mods;
-  onChange: (value: string, configName: string) => void;
+  modSelected: string;
+  onChange?: (value: string, configName: string) => void;
 }
 
 export const Config: FC<Props> = ({ modSelected, onChange }) => {
-  const [config, setConfig] = useState<{ [key: string]: unknown }>({});
+  const [config, setConfig] = useState<{ [key: string]: object }>({});
   const [collapsedGroups, setCollapsedGroups] = useState<{ [key: string]: boolean }>({});
+  const selectedMod = modSelected as keyof typeof mods;
 
   useEffect(() => {
-    if (mods[modSelected as keyof typeof mods]) {
-      setConfig(mods[modSelected as keyof typeof mods].properties);
+    if (mods[selectedMod]) {
+      setConfig(mods[selectedMod].properties);
     }
-  }, [modSelected]);
+  }, [selectedMod]);
 
   const handleChange = (propertyName: string, configKey: string, event: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = event.target.value;
@@ -24,12 +25,31 @@ export const Config: FC<Props> = ({ modSelected, onChange }) => {
       [propertyName]: {
         ...prevConfig[propertyName],
         properties: {
-          ...prevConfig[propertyName].properties,
-          [configKey]: { ...prevConfig[propertyName].properties[configKey], changedValue: newValue },
+          ...prevConfig[propertyName]?.properties,
+          [configKey]: { ...prevConfig[propertyName]?.properties?.[configKey], changedValue: newValue },
         },
       },
     }));
-    onChange(newValue, `${propertyName}.${configKey}`);
+    if (onChange) {
+      onChange(newValue, `${propertyName}.${configKey}`);
+    }
+  };
+
+  const handleColorChange = (propertyName: string, configKey: string, event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = hexToRgb(event.target.value);
+    setConfig((prevConfig) => ({
+      ...prevConfig,
+      [propertyName]: {
+        ...prevConfig[propertyName],
+        properties: {
+          ...prevConfig[propertyName]?.properties,
+          [configKey]: { ...prevConfig[propertyName]?.properties?.[configKey], changedValue: newValue },
+        },
+      },
+    }));
+    if (onChange) {
+      onChange(newValue, `${propertyName}.${configKey}`);
+    }
   };
 
   const handleMultiChange = (propertyName: string, configKey: string, event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -38,19 +58,52 @@ export const Config: FC<Props> = ({ modSelected, onChange }) => {
     setConfig((prevConfig) => ({
       ...prevConfig,
       [propertyName]: {
-        ...prevConfig[propertyName],
+        ...(prevConfig[propertyName] as object),
         properties: {
           ...prevConfig[propertyName].properties,
-          [configKey]: { ...prevConfig[propertyName].properties[configKey], changedValue: newValue },
+          [configKey]: { ...(prevConfig[propertyName].properties[configKey] as object), changedValue: newValue },
         },
       },
     }));
     onChange(newValue, `${propertyName}.${configKey}`);
-  }
+  };
 
-  function hexToRgb(hex) {
+  const exportConfig = (propertyName: string) => {
+    const configToExport = config[propertyName] as any;
+    let iniContent = `[CONFIG]\n`;
+
+    Object.entries(configToExport.properties).forEach(([key, value]: [string, any]) => {
+      const type = value.type ? value.type.toLowerCase() : 'unknown';
+
+      let formattedValue = value.changedValue || value.default;
+
+      if (Array.isArray(formattedValue)) {
+        // Format array values with square brackets
+        formattedValue = `[${formattedValue.join(', ')}]`;
+      } else if (type === 'string') {
+        formattedValue = `"${formattedValue}"`;
+      }
+
+      iniContent += `;${type}\n${key} = ${formattedValue}\n\n`;
+    });
+
+    const fileName = `${propertyName}.ini`;
+    const blob = new Blob([iniContent], { type: 'text/plain' });
+
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    link.click();
+  };
+
+  function hexToRgb(hex: string) {
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)}` : null;
+  }
+
+  function rgbToHex(rgb: string) {
+    const [r, g, b] = rgb.split(',').map((item: string) => parseInt(item.trim(), 10));
+    return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
   }
 
   const renderConfigOption = (propertyName: string, key: string, optionValue: any) => {
@@ -59,14 +112,15 @@ export const Config: FC<Props> = ({ modSelected, onChange }) => {
     switch (optionValue.type) {
       case 'string':
         if (optionValue.pattern === '^\\s*\\d{1,3}\\s*,\\s*\\d{1,3}\\s*,\\s*\\d{1,3}\\s*$') {
+          const selectedColor = optionValue.changedValue || optionValue.default;
+
           inputElement = (
             <>
               <input
                 type="color"
-                value={optionValue.changedValue || optionValue.default}
+                value={rgbToHex(selectedColor)}
                 onChange={(e) => {
-                  const rgb = hexToRgb(e.target.value);
-                  handleChange(propertyName, key, rgb);
+                  handleColorChange(propertyName, key, e);
                 }}
               />{' '}
               <label>{optionValue.changedValue || optionValue.default}</label>
@@ -104,33 +158,35 @@ export const Config: FC<Props> = ({ modSelected, onChange }) => {
         break;
         case 'array':
           if (optionValue.items && optionValue.items.anyOf) {
+            const { anyOf } = optionValue.items;
+        
             inputElement = (
               <select
                 value={optionValue.changedValue || optionValue.default}
                 onChange={(e) => handleMultiChange(propertyName, key, e)}
                 multiple
               >
-                {Object.keys(mods[modSelected].definitions).map((definitionKey, index) => {
-                  const definition = mods[modSelected].definitions[definitionKey];
-                  if (definition.enum) {
-                    return (
-                      <optgroup label={definitionKey} key={`group-${index}`}>
-                        {definition.enum.map((item) => (
-                          <option key={item} value={item}>
-                            {item}
-                          </option>
-                        ))}
-                      </optgroup>
-                    );
-                  }
-                  return null; // Handle other types if necessary
-                })}
+                {anyOf.length === 1 ? (
+                  anyOf[0].enum.map((item) => (
+                    <option key={item} value={item}>
+                      {item}
+                    </option>
+                  ))
+                ) : (
+                  anyOf.map((schema, index) => (
+                    <optgroup label={`---`} key={index}>
+                      {schema.enum.map((item: string) => (
+                        <option key={item} value={item}>
+                          {item}
+                        </option>
+                      ))}
+                    </optgroup>
+                  ))
+                )}
               </select>
             );
           }
           break;
-        
-        
       default:
         inputElement = null;
         break;
@@ -142,7 +198,7 @@ export const Config: FC<Props> = ({ modSelected, onChange }) => {
           value={optionValue.changedValue || optionValue.default}
           onChange={(e) => handleChange(propertyName, key, e)}
         >
-          {optionValue.enum.map((item) => (
+          {optionValue.enum.map((item: string) => (
             <option key={item} value={item}>
               {item}
             </option>
@@ -165,34 +221,6 @@ export const Config: FC<Props> = ({ modSelected, onChange }) => {
     }));
   };
 
-  const exportConfig = (propertyName: string) => {
-    const configToExport = config[propertyName] as any;
-    let iniContent = `[CONFIG]\n`;
-  
-    Object.entries(configToExport.properties).forEach(([key, value]: [string, any]) => {
-      const type = value.type ? value.type.toLowerCase() : 'unknown';
-  
-      let formattedValue = value.changedValue || value.default;
-  
-      if (Array.isArray(formattedValue)) {
-        // Format array values with square brackets
-        formattedValue = `[${formattedValue.join(', ')}]`;
-      } else if (type === 'string') {
-        formattedValue = `"${formattedValue}"`;
-      }
-  
-      iniContent += `;${type}\n${key} = ${formattedValue}\n\n`;
-    });
-  
-    const fileName = `${propertyName}.ini`;
-    const blob = new Blob([iniContent], { type: 'text/plain' });
-  
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = fileName;
-    link.click();
-  };  
-
   const renderProperties = () => {
     return Object.entries(config).map(([propertyName, propertyValue]) => {
       const isCollapsed = collapsedGroups[propertyName];
@@ -209,8 +237,8 @@ export const Config: FC<Props> = ({ modSelected, onChange }) => {
             <div>
               {Object.entries(propertyValue.properties).map(([key, value]: [string, any]) => (
                 <div key={key}>
-                  {renderConfigOption(propertyName, key, value)}{value.description && <p>{value.description}</p>}
-
+                  {renderConfigOption(propertyName, key, value)}
+                  {value.description && <p>{value.description}</p>}
                 </div>
               ))}
             </div>
